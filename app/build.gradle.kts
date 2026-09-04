0a83ed59c222032f10578a88756ac5128ed7bf7e
amirsepehr2020 <sepehr2sodoury@gmail.com>
2026-09-04 20:11:08 +0330

    chore: bump SEAM Player to 2.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "ir.seam.player"
         minSdk = 26
         targetSdk = 36
-        versionCode = 6
-        versionName = "2.0.0"
+        versionCode = 7
+        versionName = "2.1.0"
     }
 
     compileOptions {
